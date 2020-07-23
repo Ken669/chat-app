6c120b93b7ba4593ec4f11bc85b05f0168464186
Ken669 <hemetuzo@honeys.be>
2020-07-23 13:04:51 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,8 @@ class MessagesController < ApplicationController
   def index
     @room = Room.find(params[:room_id])
     @message = Message.new
+    @messages = @room.messages.includes(:user)
+    # binding.pry
   end
 
   def create
